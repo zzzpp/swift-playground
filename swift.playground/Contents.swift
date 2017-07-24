53369ea01cc3bc9f4838f9edac65dc1040ee7fa7
zzzp <zzzp@protonmail.com>
2017-07-25 06:41:36 +0800

    simple structure

---
+++ swift.playground/Contents.swift
@@ -0,0 +1,24 @@
+//: Playground - noun: a place where people can play
+//
+// play with various language feature of swift
+
+
+import UIKit
+
+
+// number
+var i :Int8 = 1
+let j :Int8 = 11
+i += 1
+
+// string
+
+// app
+let version = Bundle.main.infoDictionary?["CFBundleShortVersionString"] as? String
+
+// system
+var systemVersion = UIDevice.current.systemVersion
+var systemVersion2 = ProcessInfo.processInfo.operatingSystemVersion
+print(systemVersion2)
+3
+4
